20ef47002f48abc097112a4d599cc5815b9f3b10
DenisChumichov <50697478+DenisChumichov@users.noreply.github.com>
2022-07-10 19:05:36 +0300

    feature/redux-persist (#5)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
+import { AsyncStorage } from 'react-native';
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import reducers from './reducers';
 
-export const store = createStore(reducers);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
